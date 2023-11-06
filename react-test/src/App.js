1e8ab970332ff8adeb847a446e8dc47ba3417355
Lian <lcb_faust@naver.com>
2023-11-06 16:30:20 +0900

    231106 react PM

---
+++ react-test/src/App.js
@@ -0,0 +1,37 @@
+import logo from "./logo.svg";
+import React from "react";
+import "./App.css";
+
+function App() {
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+  const name = "은들";
+  return (
+    <div>
+      <form>이름 : {name}</form>
+      <apan>비밀번호 : </apan>
+      <input type="password"></input>
+      <div>
+        <button>버튼</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
